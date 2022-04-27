6c0b4a9e46771344d0359c74cf3e43d10be5094e
Hiago Prates <hiagohps360@gmail.com>
2022-04-27 13:56:47 -0300

    chore: update

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Container, Text } from './styles';
+// import { Wrapper } from './styles'; 
+
+type Props = {}
+
+const Home: FC<Props> = () => {
+  return <Container><Text>HOME</Text></Container>
+};
+
+export default Home;
