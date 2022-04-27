104eaefe6a68e29b670c02990ab303f708437e42
Hiago Prates <hiagohps360@gmail.com>
2022-04-27 15:12:58 -0300

    chore: update

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,20 @@
-/* eslint-disable  */
 import { StatusBar } from 'expo-status-bar';
 import React, { FC } from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
 import { Container, Header, TotalCars, HeaderContent } from './styles';
-import {RFValue} from 'react-native-responsive-fontsize'
 
-import Logo from '../../assets/logo.svg'
+import Logo from '../../assets/logo.svg';
 
 type Props = {};
 
 const Home: FC<Props> = () => (
   <Container>
-    <StatusBar
-      style="light"
-      translucent
-      backgroundColor='transparent'
-    />
+    <StatusBar style="light" translucent backgroundColor="transparent" />
     <Header>
       <HeaderContent>
-
-      <Logo width={RFValue(108)}  height={RFValue(12)} />
-      <TotalCars>Total 12 carros</TotalCars>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+        <TotalCars>Total 12 carros</TotalCars>
       </HeaderContent>
-
     </Header>
   </Container>
 );
