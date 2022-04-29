e32e613173f53a0b59e5f7744e9edf5a83b10d7d
Hiago Prates <hiagohps360@gmail.com>
2022-04-29 11:26:34 -0300

    feat: flat list home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { FC } from 'react';
 import { RFValue } from 'react-native-responsive-fontsize';
-import { Container, Header, TotalCars, HeaderContent } from './styles';
+import { Container, Header, TotalCars, HeaderContent, CarList } from './styles';
 
 import Logo from '../../assets/logo.svg';
+import Car from '../../components/Car';
 
 type Props = {};
 
-const Home: FC<Props> = () => (
-  <Container>
-    <StatusBar style="light" translucent backgroundColor="transparent" />
-    <Header>
-      <HeaderContent>
-        <Logo width={RFValue(108)} height={RFValue(12)} />
-        <TotalCars>Total 12 carros</TotalCars>
-      </HeaderContent>
-    </Header>
-  </Container>
-);
+const Home: FC<Props> = () => {
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 coupé',
+    rent: {
+      period: 'ao dia',
+      price: 120,
+    },
+    thumbnail:
+      'https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png',
+  };
+
+  return (
+    <Container>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <CarList
+        data={[1, 2, 3, 4, 5, 6, 7]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
+    </Container>
+  );
+};
 
 export default Home;
