3fe3752f6f5e6e825146cbdb5ae72e36794057e3
Hiago Prates <hiagohps360@gmail.com>
2022-04-29 12:25:09 -0300

    chore: added details

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,7 +3,20 @@ import React, { FC } from 'react';
 import BackButton from '../../components/BackButton';
 import ImageSlider from '../../components/ImageSlider';
 
-import { Container, Header, CarImages } from './styles';
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Price,
+  Period,
+  About,
+} from './styles';
 
 type Props = {};
 
@@ -23,6 +36,26 @@ const CarDetails: FC<Props> = () => {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 };
