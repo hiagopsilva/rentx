d70fbc5faf55b29ea0d0c704ae94d64498846413
Hiago Prates <hiagohps360@gmail.com>
2022-05-09 22:30:05 -0300

    chore: function get my car api

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CarDTO } from '../../dtos/CarDTO';
+import api from '../../services/api';
 
 import { Container } from './styles';
 
 const MyCars: React.FC = () => {
+  const [cars, setCars] = useState<CarDTO>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get('/schedules_byuser?user_id=1');
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, []);
   return <Container />;
 };
 
